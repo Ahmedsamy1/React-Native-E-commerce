fbb563e22000d3526ae4f96cdbb35b7aa24755f4
Ahmed Samy <ahmedsamyismail119@gmail.com>
2020-12-26 07:11:13 +0200

    Landing Screen Finished

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import LandingScreen from "./components/screens/LandingScreen";
+import BasketScreen from "./components/screens/BasketScreen";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator >
+    <Stack.Screen name="Landing" component={LandingScreen} />
+    <Stack.Screen name="BasketScreen" component={BasketScreen} />
+  </Stack.Navigator>
+);
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <StackNavigator></StackNavigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#D95500",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/screens/LandingScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableHighlight, TouchableWithoutFeedback } from "react-native";
+
+const LandingScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={() => navigation.navigate("BasketScreen")}>
+        <View style={styles.container2}>
+          <Image source={require("../../assets/mask.png")} />
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+export default LandingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: "#D95500",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
